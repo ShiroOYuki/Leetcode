b698ca2f0afee6bcd768eb268b19beb2bd9875eb
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2021-10-06 00:08:38 +0800

    98

---
+++ Python/98/main.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        stack = []
+        trav = root
+        prev = -float('inf')
+        while trav or stack:
+            if trav:
+                stack.append(trav)
+                trav = trav.left
+            else:
+                u = stack.pop()
+                if u:
+                    if u.val <= prev:
+                        return False
+                    prev = u.val
+                trav = u.right
+        return True
---
+++ Python/98/test.py
@@ -0,0 +1,7 @@
+a = True
+b = False
+
+a = True if a != False else False
+b = True if b != False else False
+
+print(a,b)
