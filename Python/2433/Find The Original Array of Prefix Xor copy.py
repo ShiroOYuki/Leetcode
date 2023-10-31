8886489732a6e100f1108614290f43eebe65ecae
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2023-10-31 10:05:52 +0800

    Update Find The Original Array of Prefix Xor copy.py

--- Python/2433/Find The Original Array of Prefix Xor copy.py
+++ Python/2433/Find The Original Array of Prefix Xor copy.py
@@ -1,7 +1,6 @@
 class Solution:
     def findArray(self, pref: list[int]) -> list[int]:
-        res = [pref[0]] + [pref[i] ^ pref[i-1] for i in range(1, len(pref))]
-        return res
+        return [pref[0]] + [pref[i] ^ pref[i-1] for i in range(1, len(pref))]
     
 print(Solution().findArray(pref = [5,2,0,3,1]))
         
